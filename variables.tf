13fd6c867e561353ce6c994477e554d6af2b3b25
Leonardo <66296967+tucano1306@users.noreply.github.com>
2024-08-04 20:23:47 -0400

    Update variables.tf
    MODIFICACIONES

--- variables.tf
+++ variables.tf
@@ -1,44 +1,81 @@
 variable "aws_region" {
-  description = "The AWS region to deploy resources in."
+  description = "The AWS region to deploy to"
+  type        = string
   default     = "us-west-2"
 }
 
 variable "s3_bucket_name" {
-  description = "The name of the S3 bucket to store reports."
-  default     = "hr-reports-bucket"
+  description = "The name of the S3 bucket"
+  type        = string
 }
 
 variable "dynamodb_table_name" {
-  description = "The name of the DynamoDB table for requests."
-  default     = "HRRequests"
+  description = "The name of the DynamoDB table"
+  type        = string
+  default     = "el_webo"
 }
 
 variable "lambda_function_name" {
-  description = "The name of the Lambda function."
-  default     = "HRLambdaFunction"
+  description = "The name of the Lambda function"
+  type        = string
 }
 
 variable "lambda_handler" {
-  description = "The handler for the Lambda function."
-  default     = "index.handler"
+  description = "The handler for the Lambda function"
+  type        = string
+  default     = "lambda_function.lambda_handler"
 }
 
 variable "lambda_runtime" {
-  description = "The runtime for the Lambda function."
+  description = "The runtime for the Lambda function"
+  type        = string
   default     = "python3.8"
 }
 
-variable "lambda_filename" {
-  description = "The filename of the Lambda deployment package."
-  default     = "lambda_function.zip"
-}
-
 variable "cloudwatch_event_rule_name" {
-  description = "The name of the CloudWatch Event Rule."
-  default     = "HRLambdaScheduleRule"
+  description = "The name of the CloudWatch event rule"
+  type        = string
+  default     = "hr_lambda_schedule_rule"
 }
 
 variable "cloudwatch_schedule_expression" {
-  description = "The schedule expression for the CloudWatch Event Rule."
-  default     = "rate(1 day)"
+  description = "The schedule expression for the CloudWatch event rule"
+  type        = string
+  default     = "rate(5 minutes)"
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
